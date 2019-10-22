8821407f950117eb6e1fa6c6ee160616c6a5cc5e
KatyaKulikova <e.kulikova@yahoo.com>
2019-10-21 22:11:37 -0400

    google project

---
+++ src/test/java/EntryPoint.java
@@ -0,0 +1,14 @@
+public class EntryPoint {
+    public static void main(String[] args) {
+        int number = 5;
+        Integer numberObject = 5;
+        Cat myCat = new Cat();
+        myCat.name = "Gav";
+        myCat.voice = "Meyau!";
+        Cat mraufsCat = new Cat();
+        mraufsCat.name = "Laila";
+        mraufsCat.voice = "Muuuuu!";
+        System.out.println("");
+        new Dog();
+    }
+}
