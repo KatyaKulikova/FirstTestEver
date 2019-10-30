a077147256d69972a86849d232bb57f26eba00fb
KatyaKulikova <e.kulikova@yahoo.com>
2019-10-30 14:44:12 -0400

    new zoo animal

---
+++ src/test/java/Monkey.java
@@ -0,0 +1,15 @@
+public class Monkey {
+    String name;
+    public String getName(){
+        return name;
+    }
+    int age;
+    public int getAge(){
+        return age;
+    }
+    double price;
+    public double getPrice(){
+        return price;
+    }
+
+}
